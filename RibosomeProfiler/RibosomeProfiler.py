12e73a492828a4083c513fac67b9d73cdf215ace
Jack Tierney <jack.tierney@student.ucc.ie>
2023-04-10 16:46:40 +0100

    initial project structure setup

--- RibosomeProfiler/RibosomeProfiler.py
+++ RibosomeProfiler/RibosomeProfiler.py
@@ -1,1 +1,100 @@
-"""Main module."""
+'''
+Main module for RibosomeProfiler
+Handles the command line interface and calls the appropriate functions
+
+Many different input combinations are possible. 
+
+Minimal Set:
+    -b, --bam <path> : Path to the bam file
+
+With this set the calculations will potentially less reliable and no gene feature information will be included in the output
+
+Standard Set:
+    -b, --bam <path> : Path to the bam file
+    -g, --gff <path> : Path to the gff file
+
+with this set the calculations will be more reliable and gene feature information will be included in the output
+
+Full Set:
+    -b, --bam <path> : Path to the bam file
+    -g, --gff <path> : Path to the gff file
+    -t, --transcriptome <path> : Path to the transcriptome fasta file
+
+with this set the calculations will contain the post information in its output but will take longest to run
+
+
+Optional Arguments:
+    -n, --name <str> : Name of the sample being analysed (default: filename of bam file)
+    -S, --subsample <int> : Number of reads to subsample from the bam file (default: 10000000)
+    -T, --transcripts <int> : Number of transcripts to consider (default: 100000)
+    -c, --config <path> : Path to the config file (default: config.yaml)
+
+Output:
+    --json : Output the results as a json file
+    --html : Output the results as an html file (default)
+    --csv : Output the results as a csv file
+    --all : Output the results as all of the above
+'''
+
+import argparse
+
+
+def argumnet_parser():
+    '''
+    Parse the command line arguments and return the parser object
+
+    Inputs:
+        None
+
+    Outputs:
+        parser: ArgumentParser object containing the parsed arguments
+    '''
+    parser = argparse.ArgumentParser(
+        description='''A python command-line utility for the generation of comprehensive reports on the quality of ribosome profiling (Ribo-Seq) datasets''',
+    )
+    parser.add_argument('-b', '--bam', type=str, required=True, help='Path to the bam file')
+    parser.add_argument('-g', '--gff', type=str, required=False, help='Path to the gff file')
+    parser.add_argument('-t', '--transcriptome', type=str, required=False, help='Path to the transcriptome fasta file')
+
+    parser.add_argument('-n', '--name', type=str, required=False, help='Name of the sample being analysed (default: filename of bam file)')
+    parser.add_argument('-S', '--subsample', type=int, required=False, default=10000000, help='Number of reads to subsample from the bam file (default: 10000000)')
+    parser.add_argument('-T', '--transcripts', type=int, required=False, default=100000, help='Number of transcripts to consider (default: 100000)')
+    parser.add_argument('-c', '--config', type=str, required=False, default='config.yaml', help='Path to the config file (default: config.yaml)')
+
+    parser.add_argument('--json', action='store_true', default=False, help='Output the results as a json file')
+    parser.add_argument('--html', action='store_true', help='Output the results as an html file (default)')
+    parser.add_argument('--csv', action='store_true', default=False, help='Output the results as a csv file')
+    parser.add_argument('--all', action='store_true', default=False, help='Output the results as all of the above')
+
+    return parser
+
+def main():
+    '''
+    Main function for the RibosomeProfiler command line interface
+
+    Inputs:
+        None
+
+    Outputs:
+        None
+    '''
+    parser = argumnet_parser()
+    args = parser.parse_args()
+
+    if args.all:
+        args.json = True
+        args.html = True
+        args.csv = True
+
+    if args.gff is None:
+        print("Running annotation free mode")
+
+    elif args.transcriptome is None:
+        print("Running annotation mode")
+    else:
+        print("Running sequence mode")
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ RibosomeProfiler/file_parser.py
@@ -0,0 +1,106 @@
+'''
+This script contains the functions used to load the required files in the RibosomeProfiler pipeline
+
+The functions are called by the main script RibosomeProfiler.py
+'''
+from Bio import SeqIO
+from gffutils import create_db, FeatureDB
+import pysam
+import pandas as pd
+
+def parse_fasta(fasta_path: str) -> dict:
+    '''
+    Read in the transcriptome fasta file at the provided path and return a dictionary
+
+    Inputs:
+        fasta_path: Path to the transcriptome fasta file
+
+    Outputs:
+        transcript_dict: Dictionary containing the transcript information
+    '''
+    #read in with biopython
+    transcript_dict = {}
+    for record in SeqIO.parse(fasta_path, 'fasta'):
+        transcript_dict[record.id] = record
+
+    return transcript_dict
+
+
+def parse_bam(bam_path: str) -> dict:
+    '''
+    Read in the bam file at the provided path and return a dictionary
+
+    Inputs:
+        bam_path: Path to the bam file
+
+    Outputs:
+        read_dict: Dictionary containing the read information (keys are the read names)
+    '''
+    bam_file = pysam.AlignmentFile(bam_path, 'rb')
+    read_dict = {}
+    for read in bam_file.fetch():
+        read_dict[read.query_name] = read
+
+    return read_dict
+
+
+def get_top_transcripts(read_dict: dict, num_transcripts: int) -> list:
+    '''
+    Get the top N transcripts based on the number of reads mapping to them
+
+    Inputs:
+        read_dict: Dictionary containing the read information (keys are the read names)
+        num_transcripts: Number of transcripts to consider
+
+    Outputs:
+        top_transcripts: List of the top N transcripts
+    '''
+    #get the top N transcripts based on the number of reads mapping to them
+    transcript_dict = {}
+    for read in read_dict.values():
+        if read.reference_name in transcript_dict:
+            transcript_dict[read.reference_name] += 1
+        else:
+            transcript_dict[read.reference_name] = 1
+
+    top_transcripts = sorted(transcript_dict, key=transcript_dict.get, reverse=True)[:num_transcripts]
+
+    return top_transcripts
+
+
+def subset_gff(gff_path: str, transcript_list: list, output_dir: str) -> str:
+    '''
+    Subset the GFF file to only include the transcripts in the provided list
+
+    Inputs:
+        gff_path: Path to the annotation file
+        transcript_list: List of transcripts to include in the subsetted GFF file
+
+    Outputs:
+        filtered_gff_path: GFF file containing only the transcripts in the provided list
+    '''
+    #read in with pandas
+    gff = pd.read_csv(gff_path, sep='\t', header=None, comment='#')
+    subsetted_gff = gff[gff[8].str.contains('|'.join(transcript_list))]
+    subsetted_gff.to_csv(f'{output_dir}/subsetted.gff', sep='\t', header=None, index=False)
+
+    return f'{output_dir}/subsetted.gff'
+
+
+def parse_gff(gff_path: str) -> dict:
+    '''
+    Read in the annotation file at the provided path and return a dictionary
+
+    Inputs:
+        gff_path: Path to the annotation file
+
+    Outputs:
+        gene_dict: Dictionary containing the gene information
+    '''
+    #read in with gffutils
+    db = create_db(gff_path, dbfn=':memory:', force=True, keep_order=True, merge_strategy='merge', sort_attribute_values=True)
+    gene_dict = {}
+    for gene in db.features_of_type('gene'):
+        gene_dict[gene.id] = gene
+
+    return gene_dict
