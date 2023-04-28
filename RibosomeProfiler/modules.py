5388e26aa3aea1bd058b2d72afe92bc12b0e92f5
Lukas-Wierdsma <Lukas.Wierdsma@gmail.com>
2023-04-28 12:18:45 +0100

    improved ligation bias module (N and options)

--- RibosomeProfiler/modules.py
+++ RibosomeProfiler/modules.py
@@ -6,13 +6,12 @@ This script contains the functions required to run individual modules of the Rib
 import pandas as pd
 import numpy as np
 
-def read_length_distribution(read_df: pd.DataFrame, config: dict) -> dict:
+def read_length_distribution(read_df: pd.DataFrame) -> dict:
     '''
     Calculate the read length distribution for the full dataset
 
     Inputs:
         read_df: Dataframe containing the read information
-        config: Dictionary containing the configuration information
 
     Outputs:
         dict: Dictionary containing the read length distribution
@@ -20,18 +19,22 @@ def read_length_distribution(read_df: pd.DataFrame, config: dict) -> dict:
     read_lengths, read_counts = np.unique(read_df['read_length'], return_counts=True)
     return dict(zip(read_lengths, read_counts))
 
-def ligation_bias_distribution(read_df: pd.DataFrame, config: dict) -> dict:
+def ligation_bias_distribution(read_df: pd.DataFrame, num_bases: int = 2, five_prime: bool = True) -> dict:
     '''
-    Calculate the proportion of the occurence in the first 2 nucleotides of the reads to check for ligation bias
+    Calculate the proportion of the occurence in the first or last n nucleotides of the reads to check for ligation bias
 
     Inputs:
         read_df: Dataframe containing the read information
-        config: Dictionary containing the configuration information
+        num_bases: Number of bases to be read (Default = 2) 
+        five_prime: Start at 5' end (True) or 3' end (False) of read (Default = True)
     
     Outputs:
         read_start_df: Dictionary containing the distribution of the first two nucleotides in the reads
     '''
-    two_sequence_dict = dict(read_df['sequence'].str.slice(stop=2).value_counts(normalize=True).sort_index())
-    ligation_bias_dict = {k:v for k,v in two_sequence_dict.items() if not k.startswith("N")}
-    ligation_bias_dict.update({k:v for k,v in two_sequence_dict.items() if k.startswith("N")})
+    if five_prime:
+        two_sequence_dict = dict(read_df['sequence'].str.slice(stop=num_bases).value_counts(normalize=True).sort_index())
+    else:
+        two_sequence_dict = dict(read_df['sequence'].str.slice(start=-num_bases).value_counts(normalize=True).sort_index())
+    ligation_bias_dict = {k:v for k,v in two_sequence_dict.items() if not "N" in k}
+    ligation_bias_dict.update({k:v for k,v in two_sequence_dict.items() if "N" in k})
     return ligation_bias_dict
