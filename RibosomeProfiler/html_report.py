5745147c847916ea6201ce580b46eb6b3703a1b3
Lukas-Wierdsma <Lukas.Wierdsma@gmail.com>
2023-04-28 13:47:26 +0100

    Rough draft of html report

--- RibosomeProfiler/html_report.py
+++ RibosomeProfiler/html_report.py
@@ -7,4 +7,50 @@ The functions are called by the main script RibosomeProfiler.py if the user spec
 import os
 import pandas as pd
 import plotly
+from file_parser import parse_bam
+from modules import *
+from plots import *
 
+def create_html_report(read_df: pd.DataFrame): #add variables such as outdir, filename?
+    """
+    create a html report of RibosomeProfiler containing QC plots and more
+
+    Inputs:
+        read_df: Dataframe containing the read information
+        out: Path where the html file should be stored (Default = .)
+
+    Outputs:
+        html: A html page containing the QC report from RibosomeProfiler
+    """
+    # create HTML page
+    html_page = str()
+    head = "<header>"
+    head += "<h1>Ribosome Profiler Report</h1>"
+    head += "</header>"
+    html_page += head
+
+    # Read Length Distribution
+    body = "<body>"
+    body += "<h2>Read Length Distribution</h2>"
+    body += "<p>This shows how the read lengths are distributed.</p>"
+    
+    # create plot
+    read_length_dict = read_length_distribution(read_df)
+    read_length_plot = plot_read_length_distribution(read_length_dict, dict())
+    body += plotly.io.to_html(read_length_plot, full_html=False)
+
+    # Ligation Bias
+    body += "<h2>Ligation Bias</h2>"
+    body += "<p>This shows the distribution of the first two nucleotides in the reads to check for ligation bias.</p>"
+    
+    # create plot
+    ligation_bias_dict = ligation_bias_distribution(read_df)
+    ligation_bias_plot = plot_ligation_bias_distribution(ligation_bias_dict, dict())
+    body += plotly.io.to_html(ligation_bias_plot, full_html=False)
+
+    body += "</body>"
+    html_page += body
+    
+    # save HTML page to file
+    with open("RibosomeProfiler-report.html", "w") as f: # temp outdir
+        f.write(str(html_page))
