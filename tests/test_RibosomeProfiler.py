6414d775b5a87e5a71272a878281cc2738b279ea
Lukas-Wierdsma <Lukas.Wierdsma@gmail.com>
2023-05-02 15:27:57 +0100

    added basic module tests

--- tests/test_RibosomeProfiler.py
+++ tests/test_RibosomeProfiler.py
@@ -14,6 +14,21 @@ def test_bam_parsing():
 
 def test_read_length_distribution():
     """Test read length distribution calculation"""
-    read_df = pd.read_csv("tests/test_data/parsed_test.csv")
+    read_df_pre = pd.read_csv("tests/test_data/test.csv")
+    read_df = read_df_pre.loc[read_df_pre.index.repeat(read_df_pre['count'])].reset_index(drop=True)
     read_length_dict = read_length_distribution(read_df)
-    assert read_length_dict[16] == 41
+    assert read_length_dict[29] == 4
+
+def test_ligation_bias_distribution():
+    """Test ligation bias distribution calculation"""
+    read_df_pre = pd.read_csv("tests/test_data/test.csv")
+    read_df = read_df_pre.loc[read_df_pre.index.repeat(read_df_pre['count'])].reset_index(drop=True)
+    ligation_bias_dict = ligation_bias_distribution(read_df)
+    assert ligation_bias_dict["AA"] == 0.5
+
+def test_nucleotide_composition():
+    """Test nucleotide composition calculation"""
+    read_df_pre = pd.read_csv("tests/test_data/test.csv")
+    read_df = read_df_pre.loc[read_df_pre.index.repeat(read_df_pre['count'])].reset_index(drop=True)
+    nucleotide_composition_dict = nucleotide_composition(read_df)
+    assert nucleotide_composition_dict["A"] == [0.5,0.5,0.625,0.625,0.125,0,0,0.375,0.375,0.375]
