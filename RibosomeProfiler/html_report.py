4bcb2afafac7da2d87b40a0978d518dc7a0c712b
Lukas-Wierdsma <Lukas.Wierdsma@gmail.com>
2023-05-03 17:57:41 +0100

    updated html_report to a function

--- RibosomeProfiler/html_report.py
+++ RibosomeProfiler/html_report.py
@@ -1,38 +1,52 @@
 """
-Code in this script is used to generate the HTML output report
+Code in this script is used to generate the HTML and pdf output report
 The functions are called by the main script RibosomeProfiler.py if the user specifies the --html flag
-
-NOTE: This is a draft with the goal of creating the html report through this script.
 """
 
-from file_parser import parse_bam
-from modules import *
-from plots import *
 from jinja2 import Environment, FileSystemLoader
 from datetime import datetime
+from modules import convert_html_to_pdf
 
-env = Environment(loader=FileSystemLoader("RibosomeProfiler/templates"),autoescape=False)
-out = "RibosomeProfiler_report.html"
-bam_file = input("Please provide the path to the bam file.\n")
+def generate_report(plots: dict, export_mode: str = 'html', name: str = "RibosomeProfiler_report", outdir: str = ''):
+    """
+    Generates a report of the RibosomeProfiler results with plots
 
-read_df_pre = parse_bam(bam_file, 10000)
-read_df = read_df_pre.loc[read_df_pre.index.repeat(read_df_pre['count'])].reset_index(drop=True)
-read_length_dict = read_length_distribution(read_df)
-plot_read_length_dict = plot_read_length_distribution(read_length_dict, dict())
-ligation_bias_dict = ligation_bias_distribution(read_df)
-plot_ligation_bias_dict = plot_ligation_bias_distribution(ligation_bias_dict, dict())
-nucleotide_composition_dict = nucleotide_composition(read_df)
-plot_nucleotide_composition_dict = plot_nucleotide_composition(
-    nucleotide_composition_dict, dict()
-)
-plots = [plot_read_length_dict, plot_ligation_bias_dict, plot_nucleotide_composition_dict]
+    Inputs:
+        results: A dictionary containing the results of the RibosomeProfiler analysis
+        export_mode: A string defining the mode of export: 'html','pdf' or 'both' (Default: 'html')
+        name: A string for the file name (Default: 'RibosomeProfiler_report')
+        outdir: A string for the output directory (Default: '')
 
-completion_time = datetime.now().strftime("%H:%M:%S %d/%m/%Y")
-#completion_time = "test"
+    Outputs:
+        No variables will be output
+    """
+    env = Environment(loader=FileSystemLoader("templates"),autoescape=False)
+    
+    completion_time = datetime.now().strftime("%H:%M:%S %d/%m/%Y")
 
-template = env.get_template("base.html")
-context = {"plots": plots, "datetime": completion_time}
-with open(out, mode="w", encoding="utf-8") as f:
-    f.write(template.render(context))
+    if outdir == '':
+        output = name
+    else:
+        if outdir.endswith('/') and outdir != '':
+            outdir = outdir[:-1]
+        output = outdir + "/" + name
+    
+    if export_mode == 'both':
+        filetype = ['html','pdf']
+    else:
+        filetype = [export_mode]
+    
+    template = env.get_template("base.html")
+    context = {"plots": plots, "export_mode": filetype, "datetime": completion_time}
+    jinja_render = template.render(context)
+    for filetype in export_mode:
+        if export_mode == 'html':
+            out = output + ".html"
+            with open(out, mode="w", encoding="utf-8") as f:
+                f.write(jinja_render)
+        else:
+            out = output + ".pdf"
+            convert_html_to_pdf(jinja_render, out)
 
-print(f"Done! Your report can be found in {out}")
+    
+    print(f"Done! Your report can be found in {out}")
