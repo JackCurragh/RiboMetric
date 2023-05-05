241718483b4f5401c21a9c86b74efbc97e4b1a49
Lukas-Wierdsma <Lukas.Wierdsma@gmail.com>
2023-05-05 14:13:14 +0100

    commented incomplete sequence_mode code

--- RibosomeProfiler/qc.py
+++ RibosomeProfiler/qc.py
@@ -102,8 +102,8 @@ def sequence_mode(
         "ligation_bias_distribution": ligation_bias_distribution(read_df),
         "nucleotide_composition": nucleotide_composition(read_df),
     }
-    results_dict["read_frame_distribution"] = read_frame_distribution(cds_read_df)\
-        if config["qc"]["use_cds_subset"]["read_frame_distribution"]\
-        else read_frame_distribution(read_df)
+    # results_dict["read_frame_distribution"] = read_frame_distribution(cds_read_df)\
+    #     if config["qc"]["use_cds_subset"]["read_frame_distribution"]\
+    #     else read_frame_distribution(read_df)
 
     return results_dict
