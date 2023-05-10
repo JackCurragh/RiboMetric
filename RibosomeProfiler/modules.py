b59ea9b9a9f83142e16cf5271695cf9f2445cdb9
Lukas-Wierdsma <Lukas.Wierdsma@gmail.com>
2023-05-10 11:52:18 +0100

    moved scoring function for better readability

--- RibosomeProfiler/modules.py
+++ RibosomeProfiler/modules.py
@@ -147,30 +147,6 @@ def read_frame_cull(read_frame_dict: dict, config: dict) -> dict:
     return culled_read_frame_dict
 
 
-def read_frame_score(read_frame_dict:dict) -> dict:
-    """
-    Generates scores for each read_length seperately as well as a global score
-    Can be used after read_frame_cull to calculate the global score of the region of interest
-    The calculation for this score is: 1 - sum(2nd highest peak count)/sum(highest peak count)
-    A score close to 1 has good periodicity, while a score closer to 0 has a random spread
-    
-    Inputs:
-    read_frame_dict: dictionary containing the distribution of the reading frames over the different read lengths
-    
-    Outputs:
-    scored_read_frame_dict: dictionary containing read frame distribution scores for each read length and a global score
-    """
-    scored_read_frame_dict = {}
-    highest_peak_sum, second_peak_sum = 0, 0
-    for k, inner_dict in read_frame_dict.items():
-        top_two_values = sorted(inner_dict.values(), reverse=True)[:2]
-        highest_peak_sum += top_two_values[0]
-        second_peak_sum += top_two_values[1]
-        scored_read_frame_dict[k] = 1-top_two_values[1]/top_two_values[0]
-    scored_read_frame_dict["global"] = (1-second_peak_sum/highest_peak_sum)
-    return scored_read_frame_dict
-
-
 def read_frame_distribution(a_site_df: pd.DataFrame) -> dict:
     """
     Calculate the distribution of the reading frame over the dataset
@@ -195,6 +171,30 @@ def read_frame_distribution(a_site_df: pd.DataFrame) -> dict:
             read_frame_dict[read_length] = {0:0,1:0,2:0}
         read_frame_dict[read_length][read_frame] = value
     return read_frame_dict
+
+
+def read_frame_score(read_frame_dict:dict) -> dict:
+    """
+    Generates scores for each read_length seperately as well as a global score
+    Can be used after read_frame_cull to calculate the global score of the region of interest
+    The calculation for this score is: 1 - sum(2nd highest peak count)/sum(highest peak count)
+    A score close to 1 has good periodicity, while a score closer to 0 has a random spread
+    
+    Inputs:
+    read_frame_dict: dictionary containing the distribution of the reading frames over the different read lengths
+    
+    Outputs:
+    scored_read_frame_dict: dictionary containing read frame distribution scores for each read length and a global score
+    """
+    scored_read_frame_dict = {}
+    highest_peak_sum, second_peak_sum = 0, 0
+    for k, inner_dict in read_frame_dict.items():
+        top_two_values = sorted(inner_dict.values(), reverse=True)[:2]
+        highest_peak_sum += top_two_values[0]
+        second_peak_sum += top_two_values[1]
+        scored_read_frame_dict[k] = 1-top_two_values[1]/top_two_values[0]
+    scored_read_frame_dict["global"] = (1-second_peak_sum/highest_peak_sum)
+    return scored_read_frame_dict
 
 
 def annotate_reads(a_site_df: pd.DataFrame, annotation_df: pd.DataFrame) -> pd.DataFrame:
