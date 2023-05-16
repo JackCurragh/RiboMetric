68ee95ee601bdf2aba1ae899ab68059e741e6577
Lukas-Wierdsma <Lukas.Wierdsma@gmail.com>
2023-05-16 16:52:02 +0100

    Updated ligation bias and nt composition tests

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -20,12 +20,22 @@ def test_read_length_distribution():
 
 def test_ligation_bias_distribution():
     """Test ligation bias distribution calculation"""
+    errors = []
     read_df_pre = pd.read_csv("tests/test_data/test.csv")
     read_df = read_df_pre.loc[
         read_df_pre.index.repeat(read_df_pre["count"])
     ].reset_index(drop=True)
-    ligation_bias_dict = ligation_bias_distribution(read_df)
-    assert ligation_bias_dict["AA"] == 0.5
+    ligation_bias_dict = ligation_bias_distribution(read_df,
+                                                    num_bases=2,
+                                                    five_prime=True)
+    if not ligation_bias_dict["AA"] == 0.25:
+        errors.append("Ligation bias (2 base, 5') error")
+    ligation_bias_dict = ligation_bias_distribution(read_df,
+                                                    num_bases=3,
+                                                    five_prime=False)
+    if not ligation_bias_dict["AAA"] == 0.125:
+        errors.append("Ligation bias (3 base, 3') error")
+    assert not errors, "errors occured:\n{}".format("\n".join(errors))
 
 
 def test_nucleotide_composition():
@@ -38,14 +48,15 @@ def test_nucleotide_composition():
     assert nucleotide_composition_dict["A"] == [
         0.5,
         0.5,
-        0.625,
-        0.625,
-        0.125,
+        0.75,
+        0.75,
+        0.25,
         0,
         0,
-        0.375,
-        0.375,
-        0.375,
+        0.25,
+        0.25,
+        0.25,
+        1
     ]
 
 
