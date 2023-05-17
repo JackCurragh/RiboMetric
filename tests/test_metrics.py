a8c138e91341c958e6fc51bec25d78453829e182
Jack Tierney <jack.tierney@student.ucc.ie>
2023-05-17 15:23:50 +0100

    updated metrics and tests

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -13,6 +13,8 @@ from RiboMetric.metrics import (
     read_length_distribution_metric as rld_metric,
     ligation_bias_distribution_metric as lbd_metric,
     read_frame_distribution_information_content_metric as rfd_metric,
+    triplet_periodicity_weighted_score,
+    triplet_periodicity_best_read_length_score as tpbrl_metric,
 )
 
 
@@ -48,10 +50,55 @@ def test_read_frame_distribution_metric():
     """
     Test the information content metric for read frame distribution
     """
-    read_df_pre = pd.read_csv("tests/test_data/test.csv")
-    read_df = read_df_pre.loc[
-        read_df_pre.index.repeat(read_df_pre["count"])
-    ].reset_index(drop=True)
-    read_frame_dict = read_frame_distribution(a_site_calculation(read_df))
+    read_frame_dict = {
+        27: {0: 100, 1: 5, 2: 5},
+        28: {0: 1000, 1: 50000, 2: 50},
+        29: {0: 100, 1: 2000, 2: 5},
+        30: {0: 100000, 1: 50000, 2: 50000},
+        31: {0: 100, 1: 50, 2: 500},
+        32: {0: 1000, 1: 5000, 2: 5000},
+    }
     read_frame_metric = rfd_metric(read_frame_dict)
-    assert round(read_frame_metric[29], 2) == 0.7
+    assert round(read_frame_metric[32], 2) == 0.39
+
+
+def test_read_frame_distribution_metric_best_read_length():
+    """
+    Test the information content metric for read frame distribution
+    using the best_read length score
+    """
+    read_frame_dict = {
+        27: {0: 100, 1: 5, 2: 5},
+        28: {0: 1000, 1: 50000, 2: 50},
+        29: {0: 100, 1: 2000, 2: 5},
+        30: {0: 100000, 1: 50000, 2: 50000},
+        31: {0: 100, 1: 50, 2: 500},
+        32: {0: 1000, 1: 5000, 2: 5000},
+    }
+    read_frame_metric = rfd_metric(read_frame_dict)
+    read_frame_metric_best_read_length = tpbrl_metric(
+        read_frame_metric
+    )
+    assert round(read_frame_metric_best_read_length, 2) == 0.95
+
+
+def test_triplet_periodicity_weighted_score():
+    """
+    Test the triplet periodicity weighted score
+    """
+    read_frame_dict = {
+        27: {0: 100, 1: 5, 2: 5},
+        28: {0: 1000, 1: 50000, 2: 50},
+        29: {0: 100, 1: 2000, 2: 5},
+        30: {0: 100000, 1: 50000, 2: 50000},
+        31: {0: 100, 1: 50, 2: 500},
+        32: {0: 1000, 1: 5000, 2: 5000},
+    }
+    read_frame_metric = rfd_metric(read_frame_dict)
+
+    weighted_score = triplet_periodicity_weighted_score(
+        read_frame_metric,
+        read_frame_dict,
+    )
+    assert round(weighted_score, 2) == 0.38
+
