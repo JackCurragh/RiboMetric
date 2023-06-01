90c13676d7e4d8e9f3b702c8a98f4fd0e5e10d18
Lukas-Wierdsma <Lukas.Wierdsma@gmail.com>
2023-06-01 16:29:51 +0100

    fixed read length distribution metric test

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -27,7 +27,7 @@ def test_read_length_distribution_metric():
     ].reset_index(drop=True)
     read_length_dict = read_length_distribution(read_df)
     read_length_metric = rld_metric(read_length_dict)
-    assert read_length_metric == 6.0
+    assert round(read_length_metric, 3) == 0.375
 
 
 def test_ligation_bias_distribution_metric():
