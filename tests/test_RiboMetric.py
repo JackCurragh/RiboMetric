91f3943ee9dc17fca4a293bd04ebcb2c4b718901
Lukas-Wierdsma <Lukas.Wierdsma@gmail.com>
2023-06-14 18:34:28 +0100

    added 'json_in' and 'server' to main tests

--- tests/test_RiboMetric.py
+++ tests/test_RiboMetric.py
@@ -47,7 +47,7 @@ def test_main_run():
         fasta=None,
         subsample=1000,
         transcripts=None,
-        json=None,
+        json=True,
         html=True,
         pdf=None,
         csv=None,
@@ -64,3 +64,69 @@ def test_main_run():
     # Assert that the expected output was produced
     assert 'Annotation parsed' in output
     assert 'Running modules' in output
+
+
+def test_main_run_server():
+    file_path = os.path.join(os.path.dirname(__file__), "test_data")
+    print(file_path)
+    args = Namespace(
+        command="run",
+        annotation=f"{file_path}/1000_entry_RiboMetric.tsv",
+        bam=f"{file_path}/test.bam",
+        output=f"{file_path}",
+        config=f"{file_path}/../../config.yml",
+        all=False,
+        gff=None,
+        fasta=None,
+        subsample=1000,
+        transcripts=None,
+        json=None,
+        html=None,
+        pdf=None,
+        csv=True,
+        server=True
+    )
+
+    # Redirect stdout to a StringIO object to capture output
+    sys.stdout = StringIO()
+
+    main(args)
+
+    # Get the output
+    output = sys.stdout.getvalue()
+
+    # Assert that the expected output was produced
+    assert 'Annotation parsed' in output
+    assert 'Running modules' in output
+
+
+def test_main_run_json():
+    file_path = os.path.join(os.path.dirname(__file__), "test_data")
+    print(file_path)
+    args = Namespace(
+        command="run",
+        json_in=f"{file_path}/test.json",
+        output=f"{file_path}",
+        config=f"{file_path}/../../config.yml",
+        all=False,
+        gff=None,
+        fasta=None,
+        subsample=1000,
+        transcripts=None,
+        json=None,
+        html=None,
+        pdf=True,
+        csv=None,
+    )
+
+    # Redirect stdout to a StringIO object to capture output
+    sys.stdout = StringIO()
+
+    main(args)
+
+    # Get the output
+    output = sys.stdout.getvalue()
+
+    # Assert that the expected output was produced
+    assert 'JSON' in output
+    assert 'Your pdf report can be found' in output
