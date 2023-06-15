e7f2c26f7f4ae7e422fe75d20c66a0825bdcf154
Lukas-Wierdsma <Lukas.Wierdsma@gmail.com>
2023-06-15 17:47:14 +0100

    fix for test_bam_parsing

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,6 +5,8 @@ import pandas as pd
 def test_bam_parsing():
     """Test bam parsing"""
     bam = parse_bam(
-        "tests/test_data/test.bam", 10000
+        bam_file="tests/test_data/test.bam",
+        num_reads=10000,
+        num_processes=1,
     )[0]
     assert len(bam) == 9997
