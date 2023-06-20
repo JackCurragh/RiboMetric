04ba35e8ea3acffb25624143f40a8048a5e9966a
Jack Tierney <jack.tierney@student.ucc.ie>
2023-06-20 09:10:54 +0100

    v1.7

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/JackCurragh/RiboMetric",
-    version="0.1.5",
+    version="0.1.7",
     zip_safe=False,
 )
