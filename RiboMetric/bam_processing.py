d1c4ccc65671ab0cde069735556edcdcc3a3b976
Jack Tierney <jack.tierney@student.ucc.ie>
2024-03-07 14:59:28 +0000

    auto-update

--- RiboMetric/bam_processing.py
+++ RiboMetric/bam_processing.py
@@ -228,7 +228,7 @@ def process_reads(oxbow_df: pd.DataFrame) -> pd.DataFrame:
     batch_df["count"] = pd.Series([int(query.split("_x")[-1]) if "_x" in query
                                    else 1 for query in oxbow_df["qname"]],
                                   dtype="category")
-
+    print(batch_df)
     return batch_df
 
 
