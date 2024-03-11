fdb4fb5ddb5005896bcd99980d289832ac43a05f
Jack Tierney <jack.tierney@student.ucc.ie>
2024-03-11 17:49:30 +0000

    auto-update

--- RiboMetric/modules.py
+++ RiboMetric/modules.py
@@ -332,18 +332,16 @@ def read_frame_distribution(a_site_df: pd.DataFrame) -> dict:
         # Assign frame numbers based on sorted order
         # This is for unannotated reads only. 
         # It calls the most translated frame the 0 frame
-        print(frame_counts)
-        print(sorted(frame_counts.keys()))
+
         frame_count_dict = {
             frame: idx for idx, frame in enumerate(sorted(frame_counts.keys()))
             }
-        print()
-        print(frame_count_dict)
         if read_length not in read_frame_dict:
             read_frame_dict[read_length] = {
                 0: 0, 1: 0, 2: 0
                 }
         for frame, count in frame_counts.items():
+            print(read_frame_dict[read_length], frame, read_frame_dict[read_length][frame_count_dict[frame]], count)
             read_frame_dict[read_length][frame_count_dict[frame]] += count
     return read_frame_dict
 
