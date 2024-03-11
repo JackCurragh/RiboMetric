2333d18fa9b57a306254db2e952c518e05f20bd5
Jack Tierney <jack.tierney@student.ucc.ie>
2024-03-11 17:40:07 +0000

    auto-update

--- RiboMetric/modules.py
+++ RiboMetric/modules.py
@@ -259,7 +259,6 @@ def read_frame_cull(read_frame_dict: dict, config: dict) -> dict:
     culled_read_frame_dict = read_frame_dict.copy()
     cull_list = list(culled_read_frame_dict.keys())
     for k in cull_list:
-        print(k)
         if (
             k > config["plots"]["read_frame_distribution"]["upper_limit"]
             or k < config["plots"]["read_frame_distribution"]["lower_limit"]
