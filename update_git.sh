b587d21e28e1f63d0ea31e0abea255a78387e040
Jack Tierney <jack.tierney@student.ucc.ie>
2024-06-24 11:48:49 +0100

    auto-update

--- update_git.sh
+++ update_git.sh
@@ -2,8 +2,8 @@ git add .
 git commit -m "auto-update"
 git push
 
-pip uninstall RiboMetric
-pip install git+https://github.com/JackCurragh/RiboMetric.git -y
+pip uninstall RiboMetric -y
+pip install git+https://github.com/JackCurragh/RiboMetric.git
 
 # RiboMetric run -b Calviello16/Calviello.transcriptome.sorted.bam -a Calviello16/gencode.v45_RiboMetric.tsv -S 1000000 -T 500 --all
 # RiboMetric run -b Calviello16/Calviello.transcriptome.sorted.bam -S 1000000 -T 500 --all
