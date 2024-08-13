7d1e9207ebe65b48cda54cfb39c3527bcd2ffb0a
Jack Tierney <jack.tierney@student.ucc.ie>
2024-08-13 13:43:57 +0100

    adjusted read specific offsets

--- update_git.sh
+++ update_git.sh
@@ -6,6 +6,6 @@ pip uninstall RiboMetric -y
 pip install git+https://github.com/JackCurragh/RiboMetric.git
 
 # RiboMetric run -b Calviello16/Calviello.transcriptome.sorted.bam -a Calviello16/gencode.v45_RiboMetric.tsv -S 100000 --all --global-offset 15
-RiboMetric run -b Calviello16/Calviello.transcriptome.sorted.bam -a Calviello16/gencode.v45_RiboMetric.tsv -S 100000 --all --offset-read-specific 
+RiboMetric run -b Calviello16/Calviello.transcriptome.sorted.bam -a Calviello16/gencode.v45_RiboMetric.tsv -S 100000 --all --offset-read-specific Calviello16/readspecific_offsets.tsv 
 
 # RiboMetric run -j test/Calviellotranscriptomesorted_RiboMetric.json --all
